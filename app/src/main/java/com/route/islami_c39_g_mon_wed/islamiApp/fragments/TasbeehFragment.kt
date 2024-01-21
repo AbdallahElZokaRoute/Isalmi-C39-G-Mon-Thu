5221639157d275011852bb087bc16beef1065ec5
BAB AL SAFA <emanmohamedmahmoud789@gmail.com>
2024-01-21 14:58:55 +0200

    tasbeh fragment code done.

--- app/src/main/java/com/route/islami_c39_g_mon_wed/islamiApp/fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islami_c39_g_mon_wed/islamiApp/fragments/TasbeehFragment.kt
@@ -5,12 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.route.islami_c39_g_mon_wed.databinding.FragmentQuranBinding
 import com.route.islami_c39_g_mon_wed.databinding.FragmentTasbeehBinding
 
 class TasbeehFragment : Fragment() {
 
-    lateinit var binding: FragmentTasbeehBinding
+    private lateinit var binding: FragmentTasbeehBinding
+    private lateinit var Tasbeh : List<String>
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +22,25 @@ class TasbeehFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var num = 1
+        var i = 0
+        Tasbeh = listOf("الله اكبر","الحمدلله","سبحان الله")
+        binding.counter.text = "$num"
+        binding.tasbeh.setOnClickListener{
+            if (num in 1..32){
+                binding.counter.text = "${num++}"
+                num++
+            }
+            else if (num == 33) {
+                num = 1
+                binding.counter.text = "$num"
+                if (i == 3){
+                    i = 0
+                    binding.tasbeh.text = Tasbeh[i]
+                }
+                binding.tasbeh.text = Tasbeh[i++]
+            }
+        }
 
     }
 }
