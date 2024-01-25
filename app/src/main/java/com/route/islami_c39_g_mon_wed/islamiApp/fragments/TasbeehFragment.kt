ac2392e986f154784730b65cb0c748b94318c5fd
Hossam-Elgmmal <hossam.elgmmal@gmail.com>
2024-01-25 21:07:38 +0200

    finished Tasbeeh fragment
    Signed-off-by: Hossam-Elgmmal <hossam.elgmmal@gmail.com>

--- app/src/main/java/com/route/islami_c39_g_mon_wed/islamiApp/fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islami_c39_g_mon_wed/islamiApp/fragments/TasbeehFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.route.islami_c39_g_mon_wed.R
 import com.route.islami_c39_g_mon_wed.databinding.FragmentQuranBinding
 import com.route.islami_c39_g_mon_wed.databinding.FragmentTasbeehBinding
 
@@ -12,6 +13,8 @@ class TasbeehFragment : Fragment() {
 
     private lateinit var binding: FragmentTasbeehBinding
     private var count = 0
+    private lateinit var azkarList : MutableList<String>
+    private var position = 0
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,15 +27,32 @@ class TasbeehFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.currentValue.text = "$count"
+        azkarList = resources.getStringArray(R.array.azkarArray).toMutableList()
+
         binding.currentValue.setOnClickListener {
             count = 0
             binding.currentValue.text = "$count"
         }
 
         binding.countUp.setOnClickListener {
-            count++
-            binding.currentValue.text = "$count"
+            counter()
         }
+        binding.sebhaBody.setOnClickListener{
+            binding.sebhaBody.rotation += (360/33).toFloat()
+            counter()
+        }
+    }
+    private fun counter(){
+        if (count < 3)
+            count++
+        else {
+            count = 0
+
+            position = if (position < azkarList.size -1 ) ++position else 0
+            binding.countUp.text = azkarList[position]
+        }
+        binding.currentValue.text = "$count"
+
+
     }
 }
